d5835b2031b9c92b021679409a2f7852ebd838b2
Vision-Expo <gorulevishal984@gmail.com>
2026-01-23 17:35:04 +0530

    Half precision added

--- backend/core/audio_stack.py
+++ backend/core/audio_stack.py
@@ -1,2 +1,7 @@
 import whisper
-from backend.config.setting import 
+from backend.config.setting import DEVICE
+
+def load_whisper():
+    # 'fp16=True' for 2x GPU speed. fp16 = Half Precision
+    model = whisper.load_model("base", device=DEVICE)
+    return model
