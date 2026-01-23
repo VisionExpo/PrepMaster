78c86f423accfa4e52cf4b73c37589a90f80d584
Vision-Expo <gorulevishal984@gmail.com>
2026-01-23 17:29:41 +0530

    GPU setting added

---
+++ backend/config/setting.py
@@ -0,0 +1,9 @@
+import torch
+
+def get_device():
+    if torch.cuda.is_available():
+        print(f"GPU Detected: {torch.cuda.get_device_name(0)}")
+        return "cuda"
+    else:
+        print("GPU not found. Falling back to CPU.")
+        return "cpu"
